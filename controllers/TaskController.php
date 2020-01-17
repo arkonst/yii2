c2a69045bf3d7716d51b802f4686b69ec8152580
Аркадий Викторович Кондратюк <47656367+arkonst@users.noreply.github.com>
2020-01-18 01:00:27 +0300

    HomeWork #1
    Установлен YII2 Framework.
    Создан контроллер TaskController.
    Добавлены три экшена в контроллер TaskController: actionIndex, actionTasks, actionTaskDetail.

---
+++ controllers/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class TaskController extends Controller
+{
+        public function actionIndex() {
+            return $this->render('index');
+        }
+
+        public function actionTasks() {
+            return $this->render('tasks');
+        }
+
+        public function actionTaskDetail() {
+            return $this->render('task-detail');
+        }
+}
---
+++ views/task/tasks.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<p>Это страница со списком задач</p>
+
+<ul>
+    <li><a href="/task/task-detail">Задача 1</a></li>
+    <li><a href="/task/task-detail">Задача 1</a></li>
+    <li><a href="/task/task-detail">Задача 1</a></li>
+    <li><a href="/task/task-detail">Задача 1</a></li>
+</ul>
