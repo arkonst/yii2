3e1bd466b869fc7f0582d02f13d14677e599419c
Аркадий Викторович Кондратюк <47656367+arkonst@users.noreply.github.com>
2020-02-01 01:58:27 +0300

    HomeWork#4
    Домашняя работа №4

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -3,20 +3,26 @@
 
 namespace app\controllers;
 
+use app\models\tables\Tasks;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
-
 
 class TaskController extends Controller
 {
         public function actionIndex() {
-            return $this->render('index');
+
+            $dataProvider = new ActiveDataProvider([
+                'query' => Tasks::find(),
+                'pagination' => [
+                    'pageSize' => 20,
+                ],
+            ]);
+
+            return $this->render('index', ['listDataProvider' => $dataProvider]);
+
         }
 
-        public function actionTasks() {
-            return $this->render('tasks');
-        }
-
-        public function actionTaskDetail() {
+        public function actionTaskDetail($id) {
             return $this->render('task-detail');
         }
 }
--- views/task/index.php
+++ views/task/index.php
@@ -1,4 +1,22 @@
 <?php
-?>
-<p>Это главная страница трекера задач</p>
-<a href="/task/tasks">Посмотреть все задачи</a>
+
+use yii\widgets\ListView;
+
+echo ListView::widget([
+    'dataProvider' => $listDataProvider,
+    'itemView' => '_task-card',
+    'options' => [
+        'tag' => 'div',
+        'class' => 'tasks-list',
+    ],
+    'summary' => false,
+]);
+
+//var_dump($listDataProvider);
+
+
+
+
+
+
+
